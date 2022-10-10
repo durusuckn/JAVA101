a3f763f553452c18e8ec8ac0236e92fc0f869d75
Duru Su Cakan <cakandurusu@gmail.com>
2022-10-10 17:32:11 +0300

    Odev

--- Tek_Sayi_Toplam/TekSayi/src/Main.java
+++ Tek_Sayi_Toplam/TekSayi/src/Main.java
@@ -5,16 +5,16 @@ public class Main {
         int toplam=0;
         int sayi;
 
-        //Girilen negatif bir sayı olana kadar toplayan
+        //Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
 
         do {
             System.out.println("Sayı Giriniz: ");
             sayi=index.nextInt();
-            if(sayi%2==1){
+            if(sayi%4==0){
                 toplam=toplam+sayi;
             }
         }
-        while(sayi>0);
+        while(sayi%2!=1);
 
         System.out.println("Girilen tek sayıların toplamı: "+toplam);
     }
