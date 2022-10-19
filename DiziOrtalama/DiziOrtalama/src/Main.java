a04ad3d4a699b47f48fa28ec76862b658e6b96bd
Duru Su Cakan <cakandurusu@gmail.com>
2022-10-19 13:35:48 +0300

    Harmonik

--- DiziOrtalama/DiziOrtalama/src/Main.java
+++ DiziOrtalama/DiziOrtalama/src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int [] arr={1,2,3,4,5};
+        double [] arr={1,2,3,4,5};
         double sum=0;
+        double harmonik=0;
 
         for (int i=0;i<arr.length;i++){
             sum+=arr[i];
+            harmonik+=1/arr[i];
         }
 
-        System.out.println(sum/arr.length);
+        System.out.println("Aritmetik: "+sum/arr.length);
+        System.out.println("Harmonik: "+harmonik);
 
     }
 }
